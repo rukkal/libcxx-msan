aa2cbe093309ee2fab761585daf4dea528b6d1bd
rukkal <kean.mariotti@gmail.com>
2016-07-30 09:53:32 +0200

    build.sh takes as parameter the compiler to be used

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-NUM_OF_PROCESSORS=$(cat /proc/cpuinfo | grep -c ^processor)
+if [ ! $# -eq 2 ]; then
+   echo "usage: $0 <clang c compiler> <clang c++ compiler>"
+   echo "example: $0 clang-3.8 clang++-3.8"
+   exit 1
+fi
 
+export CC=$1
+export CXX=$2
+file_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+num_of_processors=$(cat /proc/cpuinfo | grep -c ^processor)
+
+cd $file_dir
 svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
 (cd llvm/projects && svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx)
 (cd llvm/projects && svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi)
 
-mkdir build
-cd build
-export CC=clang-3.8
-export CXX=clang++-3.8
+[ -d build ] && rm -rf build
+mkdir -p build && cd build
 cmake ../llvm -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_SANITIZER=Memory
-make install-libcxx install-libcxxabi -j$NUM_OF_PROCESSORS
+make install-libcxx install-libcxxabi -j$num_of_processors
